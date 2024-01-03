9d5c695cefbeaae280c0955d7239bb747765a4eb
lukestahl <stahl.luke@web.de>
2024-01-03 17:49:49 +0100

    add shortcut generator

--- packages/shortcuts-generator/src/index.ts
+++ packages/shortcuts-generator/src/index.ts
@@ -1,4 +1,41 @@
+import { exec } from "child_process";
+import { promises as fs } from "fs";
+import os from "os";
+import path from "path";
+
 import apps from "../../../public/apps.json";
 
-export { apps };
-// generate the shortcuts for the apps
+const templateDir = path.join(__dirname, "../../../public/shortcut.template.plist");
+const shortcutDir = path.join(__dirname, "../../../public/shortcuts");
+
+const generateShortcuts = async () => {
+  const templatePlist = await fs.readFile(templateDir, "utf-8");
+  await Promise.all(
+    apps.map(async (app) => {
+      const shortcut = templatePlist.replace("{{appKey}}", app.key);
+      await fs.writeFile(path.join(shortcutDir, `/${app.name}.shortcut`), shortcut);
+      return execShellCommand(
+        `shortcuts sign -i ${path.join(shortcutDir, `/${app.name}.shortcut`.replace(/(\s+)/g, "\\$1"))} -o ${path
+          .join(shortcutDir, `/${app.name}.shortcut`)
+          .replace(/(\s+)/g, "\\$1")}`
+      );
+    })
+  );
+};
+
+const time = new Date().getTime();
+void generateShortcuts().then(() => {
+  console.log(`took ${new Date().getTime() - time}ms`);
+});
+
+const execShellCommand = (cmd: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error: any, stdout: string | PromiseLike<string>, stderr: string | PromiseLike<string>) => {
+      if (error) {
+        console.warn(error);
+        reject(error);
+      }
+      resolve(stdout ? stdout : stderr);
+    });
+  });
+};
