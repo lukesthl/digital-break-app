38e80799540f5259b6b620b72928ac2da5104a2f
lukestahl <stahl.luke@web.de>
2024-01-23 17:56:31 +0100

    format

--- packages/prettier-config/config.mjs
+++ packages/prettier-config/config.mjs
@@ -1,14 +1,11 @@
 import { fileURLToPath } from "url";
 
 /** @typedef  {import("prettier").Config} PrettierConfig */
-/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
 /** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
 
-/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+/** @type { PrettierConfig | SortImportsConfig } */
 const config = {
-  plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-  ],
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
   importOrder: [
     "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
     "^(next/(.*)$)|^(next$)",
