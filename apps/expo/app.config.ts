fbff6902ab76d36c882a7665bdcf8bcfc94e2bf9
lukestahl <stahl.luke@web.de>
2024-02-07 22:43:05 +0100

    bump app version

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Digital Break",
   slug: "digitalbreak",
-  version: "1.0.1",
+  version: "1.0.2",
   orientation: "portrait",
   icon: "./assets/images/default.png",
   scheme: "digitalbreak",
